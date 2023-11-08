0b383c1de1572959a0be21e833e2dbfb2f8fb3c0
ansanloms <ansanloms@users.noreply.github.com>
2023-11-08 20:28:19 +0900

    fix: ID が変わってない場合は player を置換しない。

--- scripts/youtube-replace-player.user.js
+++ scripts/youtube-replace-player.user.js
@@ -1,20 +1,28 @@
 // ==UserScript==
 // @name         YouTube: Replace player
 // @namespace    https://github.com/ansanloms/tampermonkey-scripts
-// @version      0.0.1
+// @description  YouTube プレイヤーの置き換え。
+// @version      0.0.2
 // @author       ansanloms
 // @match        https://www.youtube.com/watch*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
+// @downloadURL  https://raw.githubusercontent.com/ansanloms/tampermonkey-scripts/main/scripts/youtube-replace-player.user.js
 // @updateURL    https://raw.githubusercontent.com/ansanloms/tampermonkey-scripts/main/scripts/youtube-replace-player.user.js
 // ==/UserScript==
 
 window.setTimeout(() => {
+  let id;
+
   const replacePlayer = () => {
+    const newId = new URL(location.href).searchParams.get("v");
+    if (id === newId) {
+      return;
+    }
+    id = newId;
+
     const newPlayer = document.createElement("iframe");
-    newPlayer.src = `https://www.youtube.com/embed/${
-      new URL(location.href).searchParams.get("v")
-    }?autoplay=1`;
+    newPlayer.src = `https://www.youtube.com/embed/${id}?autoplay=1`;
     newPlayer.style.width = "100%";
     newPlayer.style.aspectRatio = "16 / 9";
 
