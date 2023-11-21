76eaadd671bfc5570a41d3499c30c7dc7e361829
ansanloms <ansanloms@users.noreply.github.com>
2023-11-21 16:00:07 +0900

    fix: twitter-remove-ads

--- scripts/twitter-remove-ads.user.js
+++ scripts/twitter-remove-ads.user.js
@@ -1,24 +1,26 @@
 // ==UserScript==
 // @name         Twitter: Remove ads
 // @namespace    https://github.com/ansanloms/tampermonkey-scripts
-// @version      0.0.2
+// @version      0.0.3
 // @description  Twitter のプロモツイートを削除する。
 // @author       ansanloms
 // @match        https://twitter.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=twitter.com
 // @grant        none
 // @updateURL    https://raw.githubusercontent.com/ansanloms/tampermonkey-scripts/main/scripts/twitter-remove-ads.user.js
+// @downloadURL  https://raw.githubusercontent.com/ansanloms/tampermonkey-scripts/main/scripts/twitter-remove-ads.user.js
 // ==/UserScript==
 
 (() => {
   const mutationObserver = new MutationObserver(() => {
-    [
-      ...document.querySelectorAll(
-        "[d='M19.498 3h-15c-1.381 0-2.5 1.12-2.5 2.5v13c0 1.38 1.119 2.5 2.5 2.5h15c1.381 0 2.5-1.12 2.5-2.5v-13c0-1.38-1.119-2.5-2.5-2.5zm-3.502 12h-2v-3.59l-5.293 5.3-1.414-1.42L12.581 10H8.996V8h7v7z']",
-      ),
-    ].forEach((node) => {
-      const article = node.closest("article");
-      if (article) {
-        article.style = "display: none;";
+    document.querySelectorAll("[data-testid='tweet']").forEach((tweet) => {
+      if (
+        tweet.style !== "display: none;" &&
+        Array.from(tweet.querySelectorAll("span")).some((v) =>
+          v.innerText === "Ad"
+        )
+      ) {
+        tweet.style = "display: none;";
       }
     });
   });
