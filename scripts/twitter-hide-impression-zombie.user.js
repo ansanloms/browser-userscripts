8f5c45e3f061cb77c5d0ce7f971eb21d99fba964
ansanloms <ansanloms@users.noreply.github.com>
2024-01-02 20:07:08 +0900

    add: twitter-hide-impression-zombie

---
+++ scripts/twitter-hide-impression-zombie.user.js
@@ -0,0 +1,63 @@
+// ==UserScript==
+// @name         Twitter: Hide impression zombie.
+// @namespace    https://github.com/ansanloms/tampermonkey-scripts
+// @version      0.0.1
+// @description  Twitter のインプレゾンビを非表示する。
+// @author       ansanloms
+// @match        https://twitter.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=twitter.com
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/ansanloms/tampermonkey-scripts/main/scripts/twitter-hide-impression-zombie.user.js
+// @downloadURL  https://raw.githubusercontent.com/ansanloms/tampermonkey-scripts/main/scripts/twitter-hide-impression-zombie.user.js
+// ==/UserScript==
+
+(() => {
+  const mutationObserver = new MutationObserver(() => {
+    const currentTweet = document.querySelector(
+      "[aria-label='Timeline: Conversation'] [data-testid='tweet'][tabindex='-1']",
+    );
+
+    const currentTweetScreenname = currentTweet
+      ? currentTweet.querySelector("[data-testid='User-Name'] a").href.split(
+        "/",
+      ).at(-1).trim()
+      : undefined;
+
+    if (currentTweet) {
+      document.querySelectorAll(
+        "[aria-label='Timeline: Conversation'] [data-testid='tweet']:not([tabindex='-1']):not([style='display: none;'])",
+      ).forEach((elem) => {
+        const userNameElem = elem.querySelector("[data-testid='User-Name'] a");
+
+        const screenname = userNameElem
+          ? userNameElem.href.split(
+            "/",
+          ).at(-1).trim()
+          : undefined;
+
+        // 返信元と返信先が同一アカウントなら表示。
+        if (screenname && currentTweetScreenname === screenname) {
+          return;
+        }
+
+        // 認証済アカは非表示。
+        if (
+          userNameElem &&
+          userNameElem.querySelector("[data-testid='icon-verified']")
+        ) {
+          elem.style = "display: none;";
+        }
+
+        // ユーザ名にアラビア文字が含まれる場合は非表示。
+        if (userNameElem && /[\p{scx=Arabic}]+/u.test(userNameElem.innerText)) {
+          elem.style = "display: none;";
+        }
+      });
+    }
+  });
+
+  mutationObserver.observe(document.getElementById("react-root"), {
+    childList: true,
+    subtree: true,
+  });
+})();
