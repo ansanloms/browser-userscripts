63240769b82b9d7c84c82f426139c3462bd02cc1
ansanloms <ansanloms@users.noreply.github.com>
2024-01-02 23:03:21 +0900

    fix: twitter-hide-impression-zombie

--- scripts/twitter-hide-impression-zombie.user.js
+++ scripts/twitter-hide-impression-zombie.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Twitter: Hide impression zombie.
+// @name         Twitter: Hide impression zombie
 // @namespace    https://github.com/ansanloms/tampermonkey-scripts
-// @version      0.0.1
+// @version      0.0.2
 // @description  Twitter のインプレゾンビを非表示する。
 // @author       ansanloms
 // @match        https://twitter.com/*
@@ -11,4 +11,4 @@
 // @updateURL    https://raw.githubusercontent.com/ansanloms/tampermonkey-scripts/main/scripts/twitter-hide-impression-zombie.user.js
 // ==/UserScript==
 
-new MutationObserver(()=>{let r=document.querySelector("[aria-label='Timeline: Conversation'] [data-testid='tweet'][tabindex='-1']"),a=r?.querySelector("[data-testid='User-Name'] a")?.href?.split("/").at(-1).trim();r&&document.querySelectorAll("[aria-label='Timeline: Conversation'] [data-testid='tweet']:not([tabindex='-1']):not([style='display: none;'])").forEach(e=>{let t=e.querySelector("[data-testid='User-Name'] a"),n=t?.href?.split("/").at(-1).trim();n&&a===n||(t?.querySelector("[data-testid='icon-verified']")&&(e.style="display: none;"),/[\p{scx=Arabic}]+/u.test(t?.innerText||"")&&(e.style="display: none;"))})}).observe(document.getElementById("react-root"),{childList:!0,subtree:!0});
+new MutationObserver(()=>{let e=document.querySelector("[aria-label='Timeline: Conversation'] [data-testid='tweet'][tabindex='-1']"),o=e?.querySelector("[data-testid='User-Name'] a")?.href?.split("/").at(-1).trim(),a=Number(e?.querySelector("[data-testid='reply']")?.getAttribute("aria-label")?.trim().match(/\d+/g)?.at(0)||"0"),i=Number(e?.querySelector("[data-testid='retweet']")?.getAttribute("aria-label")?.trim().match(/\d+/g)?.at(0)||"0"),n=Number(e?.querySelector("[data-testid='like']")?.getAttribute("aria-label")?.trim().match(/\d+/g)?.at(0)||"0");console.log({reply:a,retweet:i,like:n}),e&&document.querySelectorAll("[aria-label='Timeline: Conversation'] [data-testid='tweet']:not([tabindex='-1']):not([style='display: none;'])").forEach(t=>{let r=t.querySelector("[data-testid='User-Name'] a"),s=r?.href?.split("/").at(-1).trim();if(!(s&&o===s)){if(a>=100||i>=300||n>=1e3){t.style="display: none;";return}if(r?.querySelector("[data-testid='icon-verified']")){t.style="display: none;";return}if(/[\p{scx=Arabic}]+/u.test(r?.innerText||"")){t.style="display: none;";return}}})}).observe(document.getElementById("react-root"),{childList:!0,subtree:!0});
--- src/twitter-hide-impression-zombie.user.ts
+++ src/twitter-hide-impression-zombie.user.ts
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Twitter: Hide impression zombie.
+// @name         Twitter: Hide impression zombie
 // @namespace    https://github.com/ansanloms/tampermonkey-scripts
-// @version      0.0.1
+// @version      0.0.2
 // @description  Twitter のインプレゾンビを非表示する。
 // @author       ansanloms
 // @match        https://twitter.com/*
@@ -19,9 +19,27 @@
 
     const currentTweetScreenname = currentTweet?.querySelector(
       "[data-testid='User-Name'] a",
-    )?.href?.split(
-      "/",
-    ).at(-1).trim();
+    )?.href?.split("/").at(-1).trim();
+
+    const reply = Number(
+      currentTweet?.querySelector("[data-testid='reply']")?.getAttribute(
+        "aria-label",
+      )?.trim().match(/\d+/g)?.at(0) || "0",
+    );
+
+    const retweet = Number(
+      currentTweet?.querySelector("[data-testid='retweet']")?.getAttribute(
+        "aria-label",
+      )?.trim().match(/\d+/g)?.at(0) || "0",
+    );
+
+    const like = Number(
+      currentTweet?.querySelector("[data-testid='like']")?.getAttribute(
+        "aria-label",
+      )?.trim().match(/\d+/g)?.at(0) || "0",
+    );
+
+    console.log({ reply, retweet, like });
 
     if (currentTweet) {
       document.querySelectorAll(
@@ -38,16 +56,24 @@
           return;
         }
 
+        // バズってるツイートは非表示。
+        if (reply >= 100 || retweet >= 300 || like >= 1000) {
+          elem.style = "display: none;";
+          return;
+        }
+
         // 認証済アカは非表示。
         if (
           userNameElem?.querySelector("[data-testid='icon-verified']")
         ) {
           elem.style = "display: none;";
+          return;
         }
 
         // ユーザ名にアラビア文字が含まれる場合は非表示。
         if (/[\p{scx=Arabic}]+/u.test(userNameElem?.innerText || "")) {
           elem.style = "display: none;";
+          return;
         }
       });
     }
